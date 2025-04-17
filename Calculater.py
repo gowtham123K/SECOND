d99938acf4aad2614ccf3513d23caa53e7cd5722
Gowtham <gowthamushadevi@gmail.com>
2025-04-17 11:06:53 +0530

    all the files

---
+++ Calculater.py
@@ -0,0 +1,3 @@
+a = int(input("Enter first num:"))
+b = int(input("enter second num:"))
+print("sum", a+b)
---
+++ test_calculater.py
@@ -0,0 +1,6 @@
+import Calculater
+def test_cases():
+    assert(2,3)
+
+test_cases()
+print("all the cases passed")
